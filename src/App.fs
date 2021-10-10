82f0f316d7d71351bece759121bfb1a996089065
Angel D. Munoz <scythes.lol@gmail.com>
2021-10-10 18:50:56 -0500

    first commit

---
+++ src/App.fs
@@ -0,0 +1,18 @@
+﻿module App
+
+open Sutil
+open Sutil.DOM
+open Sutil.Attr
+open Sutil.Styling
+
+open type Feliz.length
+
+let view () =
+    Html.app [
+        Html.header []
+        Html.main [
+
+
+        ]
+        Html.footer []
+    ]
